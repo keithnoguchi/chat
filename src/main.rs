0fbdf6e92109f40cdec5da2a5af78df726aec119
Keith Noguchi <keith.noguchi@gmail.com>
2020-04-13 22:14:41 -0700

    src/main.rs: Listen and bind to the local address

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,11 @@ use std::{
     error,
 };
 
+use async_std::{
+    net::{TcpListener, ToSocketAddrs},
+    task,
+};
+
 type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
 
 static ADDR: &str = "localhost:8000";
@@ -14,6 +19,11 @@ fn main() -> Result<()> {
         .next()
         .map(|addr| addr.parse().unwrap_or_else(|_| ADDR.to_string()))
         .unwrap_or_else(|| ADDR.to_string());
-    println!("{}", addr);
+    task::block_on(server(addr))
+}
+
+async fn server<A: ToSocketAddrs>(addr: A) -> Result<()> {
+    let listener = TcpListener::bind(addr).await?;
+    println!("{:?}", listener.local_addr()?);
     Ok(())
 }
