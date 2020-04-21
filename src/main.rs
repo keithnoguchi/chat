a481200478d634d00907c68022ae3f802818dc22
Keith Noguchi <keith.noguchi@gmail.com>
2020-04-20 21:51:11 -0700

    src/main: Listen on the socket

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+//! Async chat application
+use std::env::args;
+
+use async_std::{
+    net::{TcpListener, ToSocketAddrs},
+    task,
+};
+use futures::stream::StreamExt;
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+const ADDR: &str = "[::1]:8000";
+
+fn main() -> Result<()> {
+    let mut args = args().skip(1);
+    let addr = args
+        .next()
+        .map(|addr| addr.parse().unwrap_or(ADDR.to_string()))
+        .unwrap_or(ADDR.to_string());
+    task::block_on(server(addr))
+}
+
+async fn server<A: ToSocketAddrs>(addr: A) -> Result<()> {
+    let listener = TcpListener::bind(addr).await?;
+    println!("addr={:?}", listener.local_addr());
+
+    while let Some(s) = listener.incoming().next().await {
+        match s {
+            Err(err) => {
+                eprintln!("accept error: {}", err);
+            }
+            Ok(s) => {
+                println!("remote addr={:?}", s.peer_addr());
+            }
+        }
+    }
+    Ok(())
+}
