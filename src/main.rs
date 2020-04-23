5eed177370beb321396fabb1f42b180c161fa1d6
Keith Noguchi <keith.noguchi@gmail.com>
2020-04-22 23:03:41 -0700

    src/main: Add broker task

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 //! chat server
 use async_std::{
     net::{TcpListener, TcpStream, ToSocketAddrs},
-    task,
+    sync::Arc,
+    task::{self, TaskId},
 };
-use futures_util::stream::StreamExt;
+use futures_util::{
+    stream::StreamExt,
+    sink::SinkExt,
+};
+use futures_channel::mpsc::{unbounded, UnboundedSender, UnboundedReceiver};
 use std::{
     error::Error,
     env::args,
@@ -11,6 +16,8 @@ use std::{
 };
 
 type Result<T> = result::Result<T, Box<dyn Error + Send + Sync + 'static>>;
+type Sender<T> = UnboundedSender<T>;
+type Receiver<T> = UnboundedReceiver<T>;
 
 const ADDR: &str = "[::1]:8000";
 
@@ -23,9 +30,18 @@ fn main() -> Result<()> {
     task::block_on(listener(addr))
 }
 
+#[derive(Debug)]
+enum Event {
+    Join(TaskId, Arc<TcpStream>),
+    Leave(TaskId),
+    Message(TaskId, String),
+}
+
 async fn listener<A: ToSocketAddrs>(addr: A) -> Result<()> {
     let listener = TcpListener::bind(addr).await?;
     let mut readers = vec![];
+    let (tx, rx) = unbounded();
+    let broker = task::spawn(broker(rx));
 
     while let Some(stream) = listener.incoming().next().await {
         match stream {
@@ -33,17 +49,29 @@ async fn listener<A: ToSocketAddrs>(addr: A) -> Result<()> {
                 eprintln!("accept error: {}", err);
             }
             Ok(stream) => {
-                readers.push(task::spawn(reader(stream)));
+                readers.push(task::spawn(reader(tx.clone(), stream)));
             }
         }
     }
     while let Some(reader) = readers.pop() {
         reader.await?;
     }
+    drop(tx);
+    broker.await
+}
+
+async fn broker(mut readers: Receiver<Event>) -> Result<()> {
+    while let Some(event) = readers.next().await {
+        eprintln!("{:?}", event);
+    }
     Ok(())
 }
 
-async fn reader(stream: TcpStream) -> Result<()> {
+async fn reader(mut broker: Sender<Event>, stream: TcpStream) -> Result<()> {
+    let id = task::current().id();
+    let stream = Arc::new(stream);
     eprintln!("peer={:?}", stream.peer_addr().unwrap());
+    broker.send(Event::Join(id, Arc::clone(&stream))).await?;
+    broker.send(Event::Leave(id)).await?;
     Ok(())
 }
